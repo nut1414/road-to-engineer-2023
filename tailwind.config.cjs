2abf2b86f89c8d2350ee6f2041db8b661f419ab4
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2022-12-16 14:07:42 +0700

    fix: tailwind color not showing up

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,15 +5,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    screens: {},
-    colors: {
-      'bloodred': {
-        100: '#C12121',
-        200: '#8F0202',
-        300: '#5E0505', 
+    extend: {
+      colors: {
+        'bloodred': {
+          100: '#C12121',
+          200: '#8F0202',
+          300: '#5E0505', 
+        }
       }
-    }
+    },
+    screens: {},
+    
   },
   plugins: [],
 }
