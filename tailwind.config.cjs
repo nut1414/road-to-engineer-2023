91d7cb7190d76a147b6fcd4c5c6aa596b814689c
Qandle <unmistakework@hotmail.com>
2022-12-28 05:14:51 +0700

    chore: add font db heavent

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./index.html",
@@ -6,16 +8,25 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans:['db-heaventcond', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         'bloodred': {
           100: '#C12121',
           200: '#8F0202',
           300: '#5E0505', 
+        },
+        'juicy': {
+          100: '#E46E18',
+          200: '#D7560D',
+          300: '#8C2B0C',
         }
       }
     },
-    screens: {},
     
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
