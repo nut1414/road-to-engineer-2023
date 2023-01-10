deca5996aeaa276dc59af7db100eb509f2ac32cf
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2023-01-10 13:37:43 +0700

    fix: animation some (#61)
    * :sparkles:add: Initial Timeline & Image
    
    * Revert ":sparkles:add: Initial Timeline & Image"
    
    * :sparkles:add: Adding Background
    
    * feat: separate section
    
    * add bg to main-section and some text with responsive
    
    * add font and basic responsive text
    
    * add all element pic with responsive
    
    * add gsap
    
    * add basic car animation
    
    * add all animation hover idle text reveal
    
    * change ease function
    
    * add snapping
    
    * cleen up
    
    * remove snap because it suck
    
    * fix: delete old svg
    
    * fix: importing preact instead of react
    
    * fix: delete unneccesary react import
    
    * fix: conflicting font and delete old asset
    
    * add to the top button and fix car appear prematurly
    
    * event section with scroll animation
    
    * add actual text in dummy text
    
    * add: redirect button to facebook page
    
    * fix: href replace
    
    * add: link to existing page remove unnessary  try to clamp mobile version
    
    * change: mobile car motion path and responsize mimimum size
    
    * fixed: reponsive webkit
    
    * fix: debounce error
    
    * fix: disable normalize scroll for performance
    
    Co-authored-by: Chaiyapat Meeying <chaiyapatoam12@gmail.com>
    Co-authored-by: Chaiyapat Oam <83873103+ChaiyapatOam@users.noreply.github.com>
    Co-authored-by: Qandle <unmistakework@hotmail.com>
    Co-authored-by: CaMel <hackball08@gmail.com>

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -11,6 +11,8 @@ import BTNToTheTop from "./BTNToTheTop";
 import { Link } from "react-router-dom";
 
 gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
+// ScrollTrigger.normalizeScroll(true)
+ScrollTrigger.config({ ignoreMobileResize: true})
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
@@ -38,11 +40,11 @@ export default function useWindowDimensions() {
 }
 export const debounce = (fn, ms) => {
   let timer
-  return _ => {
+  return (...args) => {
     clearTimeout(timer)
     timer = setTimeout(_=>{
       timer = null
-      fn.apply(this, arguments)
+      fn.apply(this,args)
     }, ms)
   };
 }
@@ -64,19 +66,38 @@ export const Timeline = ({scrollref}) => {
       window.removeEventListener('resize', debouncedHandleResize)
     }
   }, [])
-  
-  let varMaringin = width >= 1280 ? Math.min((((width-1280)/148)*55)+170, 225): width >= 768 ? 165 : width > 0 ? Math.min(((width-320)/60)*90+60,150) : 0
-  let varPadding = width >= 1280 ? Math.min((((width-1280)/148)*3)+11, 14): width >= 768 ? 25 : width > 0 ? Math.min(((width-320)/60)*2+10,12) : 0
-  let mobileMargin = width < 768 ? 60 : 0
+  const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+  let varMargin
+  let varPadding
+  let mobileMargin
+  let para
+  let header
+  let header2
+  if(!isSafari){
+    varMargin = width >= 1280 ? Math.min((((width-1280)/148)*55)+170, 225): width >= 768 ? 165 : width > 0 ? Math.min(((width-320)/60)*90+60,150) : 0
+    varPadding = width >= 1280 ? Math.min((((width-1280)/148)*3)+11, 14): width >= 768 ? 25 : width > 0 ? Math.min(((width-320)/60)*2+10,12) : 0
+    mobileMargin = width < 768 ? 60 : 0
+    para = "text-xl md:text-3xl xl:text-4xl mb-2.5 max-w-[250px] md:max-w-md xl:max-w-xl"
+    header = "text-bloodred-200 text-[42px] md:text-7xl xl:text-8xl font-bold"
+    header2 = "text-bloodred-200 text-[32px] md:text-7xl xl:text-8xl font-bold"
+  }else{
+    varMargin = width >= 1280 ? Math.min((((width-1280)/148)*55)+170, 225): width >= 768 ? 165 : width > 0 ? Math.min(((width-320)/60)*90+60,150) : 0
+    varPadding = width >= 1280 ? Math.min((((width-1280)/148)*3)+11, 14): width >= 768 ? 25 : width > 0 ? Math.min(((width-320)/60)*3.5+7.5,11) : 0
+    mobileMargin = width >= 1280 ? 20 : width < 768 ? width >= 375 ? Math.min(((width-375)/55)*60,60) : Math.min(((width)/55)*60,60) : 0
+    para = "text-xl md:text-3xl xl:text-4xl mb-2.5 max-w-[230px] md:max-w-md xl:max-w-xl"
+    header = "text-bloodred-200 text-[38px] md:text-7xl xl:text-8xl font-bold"
+    header2 = "text-bloodred-200 text-[28px] md:text-6xl xl:text-7xl font-bold"
+  }
   useLayoutEffect(() => {
 
     let buttons = document.querySelectorAll("#button")
     buttons.forEach((button)=>{
-      const hover = gsap.set("#shape", {
-        duration: .5,
-        display: "block",
-        paused: true
-      })
+      // console.log(button)
+      // const hover = gsap.set("#shape", {
+      //   duration: .5,
+      //   display: "block",
+      //   paused: true
+      // })
       const btnHover = gsap.to(button, {
         duration: .5,
         background: "#C12121",
@@ -349,36 +370,36 @@ export const Timeline = ({scrollref}) => {
               <ElementMobile/>
             </div>
             <div className="absolute translate-x-8 md:translate-x-28 xl:translate-x-[-40%] xl:left-1/2" style={`padding: ${varPadding}rem 0`}>
-              <section id="section" className="flex-1 font-DB-Heavent" style={`margin-bottom: ${varMaringin}px`}>
+              <section id="section" className="flex-1 font-DB-Heavent" style={`margin-bottom: ${varMargin}px`}>
                 <div id="wrapper" className="overflow-hidden relative">
-                  <h2 id="header" className="text-bloodred-200 text-[42px] md:text-7xl xl:text-8xl font-bold">Open House</h2>
+                  <h2 id="header" className={header}>Open House</h2>
                 </div>
                 <hr id="divider" className="mt-0.5 mb-3.5 border-t-[6px] border-bloodred-100 w-20 md:w-40 xl:w-80"/>
-                <p id="detail" className="text-xl md:text-3xl xl:text-4xl mb-2.5 max-w-[250px] md:max-w-md xl:max-w-xl">กิจกรรมที่เปิดให้นักเรียน คุณครู และผู้ปกครอง สัมผัสบรรยากาศ รับรู้ถึงสิ่งอำนวยความสะดวก ของคณะฯ และทำกิจกรรมร่วมกันกับนักศึกษาใน ภาควิชาต่าง ๆ</p>
-                <button onClick={()=>{window.open("", "_blank")}} id="button" className="translate-x-14 md:translate-x-32 xl:translate-x-0 shadow-[inset_5px_5px_5px_0_rgba(0,0,0,0.4)] border-[5.5px] md:border-[8px] xl:border-[12px] border-bloodred-200 font-bold rounded-full px-4 md:px-8 xl:px-16 text-[28px] md:text-[32px] xl:text-[40px] text-bloodred-200 bg-white">เว็บไซต์กิจกรรม</button>
+                <p id="detail" className={para}>กิจกรรมที่เปิดให้นักเรียน คุณครู และผู้ปกครอง สัมผัสบรรยากาศ รับรู้ถึงสิ่งอำนวยความสะดวก ของคณะฯ และทำกิจกรรมร่วมกันกับนักศึกษาใน ภาควิชาต่าง ๆ</p>
+                <button onClick={()=>{window.open("/openhouse", "_blank")}} id="button" className="translate-x-14 md:translate-x-32 xl:translate-x-0 shadow-[inset_5px_5px_5px_0_rgba(0,0,0,0.4)] border-[5.5px] md:border-[8px] xl:border-[12px] border-bloodred-200 font-bold rounded-full px-4 md:px-8 xl:px-16 text-[28px] md:text-[32px] xl:text-[40px] text-bloodred-200 bg-white">เว็บไซต์กิจกรรม</button>
               </section>
-              <section id="section" className="flex-1 font-DB-Heavent" style={`margin-bottom: ${varMaringin+40}px`}>
+              <section id="section" className="flex-1 font-DB-Heavent" style={`margin-bottom: ${varMargin+mobileMargin}px`}>
                 <div id="wrapper" className="overflow-hidden relative">
-                  <h2 id="header" className="text-bloodred-200 text-[42px] md:text-7xl xl:text-8xl font-bold">การแข่งขันนวัตกรรม</h2>
+                  <h2 id="header" className={header}>การแข่งขันนวัตกรรม</h2>
                 </div>
                 <hr id="divider" className="mt-0.5 mb-3.5 border-t-[6px] border-bloodred-100 w-20 md:w-40 xl:w-80"/>
-                <p id="detail" className="text-xl md:text-3xl xl:text-4xl mb-2.5 max-w-[250px] md:max-w-md xl:max-w-xl">การแข่งขันเพื่อสนับสนุนการสร้างสรรค์และ ส่งเสริมการพัฒนาสิ่งประดิษฐ์ หรือผลงานวิจัย เชิงประยุกต์ทางด้านวิทยาศาสตร์ และเทคโนโลยี ที่ตอบสนองการใช้งานได้จริง !</p>
+                <p id="detail" className={para}>การแข่งขันเพื่อสนับสนุนการสร้างสรรค์และ ส่งเสริมการพัฒนาสิ่งประดิษฐ์ หรือผลงานวิจัย เชิงประยุกต์ทางด้านวิทยาศาสตร์ และเทคโนโลยี ที่ตอบสนองการใช้งานได้จริง !</p>
                 <button onClick={()=>{window.open("https://www.facebook.com/profile.php?id=100087709743668", "_blank")}} id="button" className="translate-x-14 md:translate-x-32 xl:translate-x-80 shadow-[inset_5px_5px_5px_0_rgba(0,0,0,0.4)] border-[5.5px] md:border-[8px] xl:border-[12px] border-bloodred-200 font-bold rounded-full px-4 md:px-8 xl:px-16 text-[28px] md:text-[32px] xl:text-[40px] text-bloodred-200 bg-white">ติดตามข่าวสาร</button>
               </section>
-              <section id="section" className="flex-1 font-DB-Heavent" style={`margin-bottom: ${varMaringin}px`}>
+              <section id="section" className="flex-1 font-DB-Heavent" style={`margin-bottom: ${varMargin}px`}>
                 <div id="wrapper" className="overflow-hidden relative">
-                  <h2 id="header" className="text-bloodred-200 text-[32px] md:text-7xl xl:text-8xl font-bold whitespace-nowrap">การแข่งขันตอบปัญหาวิชาการ</h2>
+                  <h2 id="header" className={header2}>การแข่งขันตอบปัญหาวิชาการ</h2>
                 </div>
                 <hr id="divider" className="mt-0.5 mb-3.5 border-t-[6px] border-bloodred-100 w-20 md:w-40 xl:w-80"/>
-                <p id="detail" className="text-xl md:text-3xl xl:text-4xl mb-2.5 max-w-[250px] md:max-w-md xl:max-w-xl">การแข่งขันที่ทดสอบความรู้ความสามารถทางด้านวิศวกรรมศาสตร์ วิทยาศาสตร์ และเทคโนโลยี ให้เกิดทักษะและตระหนักถึงองค์ความรู้ที่สามารถ นำไปใช้ให้เกิดประโยชน์ต่อไปได้ !</p>
+                <p id="detail" className={para}>การแข่งขันที่ทดสอบความรู้ความสามารถทางด้านวิศวกรรมศาสตร์ วิทยาศาสตร์ และเทคโนโลยี ให้เกิดทักษะและตระหนักถึงองค์ความรู้ที่สามารถ นำไปใช้ให้เกิดประโยชน์ต่อไปได้ !</p>
                 <button onClick={()=>{window.open("https://www.facebook.com/profile.php?id=100087709743668", "_blank")}} id="button" className="translate-x-14 md:translate-x-32 xl:translate-x-0 shadow-[inset_5px_5px_5px_0_rgba(0,0,0,0.4)] border-[5.5px] md:border-[8px] xl:border-[12px] border-bloodred-200 font-bold rounded-full px-4 md:px-8 xl:px-16 text-[28px] md:text-[32px] xl:text-[40px] text-bloodred-200 bg-white">ติดตามข่าวสาร</button>
               </section>
               <section id="section" className="flex-1 font-DB-Heavent">
                 <div id="wrapper" className="overflow-hidden relative">
-                  <h2 id="header" className="text-bloodred-200 text-[42px] md:text-7xl xl:text-8xl font-bold">Bangmod Hackathon</h2>
+                  <h2 id="header" className={header}>Bangmod Hackathon</h2>
                 </div>
                 <hr id="divider" className="mt-0.5 mb-3.5 border-t-[6px] border-bloodred-100 w-20 md:w-40 xl:w-80"/>
-                <p id="detail" className="text-xl md:text-3xl xl:text-4xl mb-2.5 max-w-[250px] md:max-w-md xl:max-w-xl">การแข่งขันเพื่อสนับสนุนการแก้ไขปัญหาด้วยการ เขียนโปรแกรมทางคอมพิวเตอร์ผ่านภาษาซี และ ส่งเสริมให้ได้มีโอกาสพัฒนาศักยภาพ การคิด วิเคราะห์ และการลงมือทำจริง !</p>
+                <p id="detail" className={para}>การแข่งขันเพื่อสนับสนุนการแก้ไขปัญหาด้วยการ เขียนโปรแกรมทางคอมพิวเตอร์ผ่านภาษาซี และ ส่งเสริมให้ได้มีโอกาสพัฒนาศักยภาพ การคิด วิเคราะห์ และการลงมือทำจริง !</p>
                 <button onClick={()=>{window.open("https://bangmodhackathon.com/", "_blank")}} id="button" className="translate-x-14 md:translate-x-32 xl:translate-x-80 shadow-[inset_5px_5px_5px_0_rgba(0,0,0,0.4)] border-[5.5px] md:border-[8px] xl:border-[12px] border-bloodred-200 font-bold rounded-full px-4 md:px-8 xl:px-16 text-[28px] md:text-[32px] xl:text-[40px] text-bloodred-200 bg-white">เว็บไซต์กิจกรรม</button>
               </section>
             </div>
