c7eab12a9877c99c569e9456d8e0a45a1720f12b
Kim Chanidapa <62463618+Qandle@users.noreply.github.com>
2023-01-10 14:03:59 +0700

    fix:  edit detail (#63)

--- src/components/Another.jsx
+++ src/components/Another.jsx
@@ -11,12 +11,15 @@ export const Another = () => {
           <EventCard
             title="Concert"
             image={concert}
-            detail="Lorem ipsum dolor sit amet consectetur adipisicing."
+            detail="มีกิจกรรม Folk song เล่นดนตรีจากพี่ ๆ วิศวฯ บางมด ที่จะเล่นตลอดทั้งวัน บริเวณใต้ตึก
+            CB4 คณะวิศวกรรมศาสตร์ น้อง ๆ คนไหนอยากมาฟังพี่ ๆ เล่นดนตรี ห้ามพลาดเด็ดขาด!
+            ซุ้มอาหาร"
           />
           <EventCard
             title="ซุ้มอาหาร"
             image={food}
-            detail="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum!"
+            detail="มีซุ้มอาหารและเครื่องดื่มจากพี่ ๆ วิศวฯ บางมด รอบบริเวณคณะวิศวกรรมศาสตร์ เพื่อให้บริการน้อง ๆ ระหว่างเดินดูคณะตลอดทั้งวัน 
+            ตั้งแต่เวลา 09.00 - 18.00 น. บอกเลยว่าพี่ ๆ คัดสรรมาเป็นอย่างดี เพราะฉะนั้นไม่มาไม่ได้แล้ว !"
           />
         </div>
       </div>
