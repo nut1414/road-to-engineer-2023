7c1ef15f420a333c9bd729d4f7a3cdc816a5c3bd
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2023-01-13 11:49:00 +0700

    add: profile page (#71)
    Co-authored-by: Kim Chanidapa <Qandle@users.noreply.github.com>

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,13 @@
+import { FooterProfile } from "../components/Profile/FooterProfile"
+import { MiddleProfile } from "../components/Profile/MiddleProfile"
+import { TopProfile } from "../components/Profile/TopProfile"
+
 export const ProfilePage = () => {
   return (
-    <>
-      
-    </>
+    <div className="min-h-screen flex flex-col justify-between scrollbar-thin scrollbar-thumb-juicy-100 scrollbar-track-juicy-100/25 overflow-y-scroll scrollbar-thumb-rounded-full scrollbar-track-rounded-full">
+      <TopProfile />
+      <MiddleProfile />
+      <FooterProfile />
+    </div>
   )
 }
