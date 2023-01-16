cd0c5177eb7c5a90c203bdccf608b42c8e1f6255
Qandle <unmistakework@hotmail.com>
2023-01-16 21:22:32 +0700

    feat: e-stamp template

--- src/components/Estamp/Carousel.jsx
+++ src/components/Estamp/Carousel.jsx
@@ -1,28 +1,79 @@
+const Part = ({ title, stamps, type }) => {
+  return (
+    <>
+      <div className="h-fit">
+        <h1 className="text-center text-3xl text-bloodred-300 font-bold py-2">
+          {title}
+        </h1>
+        <div className="grid grid-cols-4 gap-4 h-full">
+          {stamps.map((e) => {
+            return (
+              <>
+                <div>
+                  <div className="relative flex justify-center items-center h-20">
+                    {
+                      (type === "exhibition" &&
+                        <>
+                          <img src="/image/stampframe.svg" alt="" className="absolute h-16" />
+                          <h2 className="absolute text-bloodred-300 font-bold ">{e}</h2>
+                        </>
+                      )
+                      ||
+                      (
+                        type === "workshop" &&
+                        <>
+                          <img src="/image/stampframews.svg" alt="" className="absolute h-16" />
+                          <h2 className="absolute text-[0.75rem] sm:text-[1rem] text-white bottom-[57%] sm:bottom-[56%] ">{e}</h2>
+                        </>
+                      )
+                    }
+                  </div>
+                </div>
+              </>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  )
+}
+
+const stamps1 = [
+  "RC",
+  "CPE",
+  "CE",
+  "ME",
+  "PE",
+  "EE",
+]
+
 export const Carousel = ({ items }) => {
 
   return (
     <div className="container mx-auto bg-white h-256 w-full mb-6 rounded-2xl p-6 md:p-10 flex flex-col shadow-lg shadow-black/50 text-gray-500">
-      <div className="carousel w-full">
-        <div id="item1" className="carousel-item w-full">
-          <img src="https://placeimg.com/800/200/arch" className="w-full" />
-        </div> 
-        <div id="item2" className="carousel-item w-full">
-          <img src="https://placeimg.com/800/200/arch" className="w-full" />
-        </div> 
-        <div id="item3" className="carousel-item w-full">
-          <img src="https://placeimg.com/800/200/arch" className="w-full" />
-        </div> 
-        <div id="item4" className="carousel-item w-full">
-          <img src="https://placeimg.com/800/200/arch" className="w-full" />
+      <div className="carousel w-full h-fit">
+        <div id="item1" className="carousel-item w-full h-fit flex flex-col">
+          <Part title="ซุ้มกิจกรรม" stamps={stamps1} type="exhibition" />
+          <Part title="ซุ้มกิจกรรม" stamps={stamps1} type="workshop" />
+          {/* <img src="https://placeimg.com/800/200/arch" className="w-full" /> */}
         </div>
-      </div> 
+        <div id="item2" className="carousel-item w-full">
+          {/* <img src="https://placeimg.com/800/200/arch" className="w-full" /> */}
+        </div>
+        <div id="item3" className="carousel-item w-full">
+          {/* <img src="https://placeimg.com/800/200/arch" className="w-full" /> */}
+        </div>
+        <div id="item4" className="carousel-item w-full">
+          {/* <img src="https://placeimg.com/800/200/arch" className="w-full" /> */}
+        </div>
+      </div>
       <div className="flex justify-center w-full py-2 gap-2">
-        <a href="#item1" className="btn btn-xs">1</a> 
-        <a href="#item2" className="btn btn-xs">2</a> 
-        <a href="#item3" className="btn btn-xs">3</a> 
+        <a href="#item1" className="btn btn-xs"></a>
+        <a href="#item2" className="btn btn-xs">2</a>
+        <a href="#item3" className="btn btn-xs">3</a>
         <a href="#item4" className="btn btn-xs">4</a>
       </div>
     </div>
-    
+
   )
 }
--- src/components/Estamp/MiddleEstamp.jsx
+++ src/components/Estamp/MiddleEstamp.jsx
@@ -2,7 +2,8 @@ import { Carousel } from "./Carousel"
 
 export const MiddleEstamp = () => {
   return (
-    <div className="p-2 bg-juicy-100 grow">
+    <div className="p-2 bg-juicy-100 grow flex flex-col justify-center items-center">
+      <h1 className="text-white text-3xl lg:text-4xl font-bold tracking-wide">My E-Stamp</h1>
       <Carousel/>
     </div>
   )
