ff4c81ff3de7e36a766aabc8be37f13b5aa52b3f
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2023-01-18 13:24:38 +0700

    feat: estamp page ui (#75)
    * add: estamp layout (no stamp)
    
    * feat: e-stamp template
    
    * feat(estamp): UI Stamp and footer rules
    
    * fix: typo on estamp rules
    
    * fix: redirect to profile instead of the front page
    
    Co-authored-by: Qandle <unmistakework@hotmail.com>

---
+++ src/components/Estamp/schema/allStampsBox.js
@@ -0,0 +1,127 @@
+export const allStampBox = [
+  {
+    name: "rc_norm",
+    catagory: "department_stamp",
+    done: false
+  },
+  {
+    name: "ce_norm",
+    catagory: "department_stamp",
+    done: false
+  },
+  {
+    name: "ce_work",
+    catagory: "workshop_stamp",
+    done: false
+  },
+  {
+    name: "me_norm",
+    catagory: "department_stamp",
+    done: false
+  },
+  {
+    name: "me_work",
+    catagory: "workshop_stamp",
+    done: false
+  },
+  {
+    name: "pemce_norm",
+    catagory: "department_stamp",
+    done: false
+  },
+  {
+    name: "pemce_work",
+    catagory: "workshop_stamp",
+    done: false
+  },
+  {
+    name: "ee_norm",
+    catagory: "department_stamp",
+    done: false
+  },
+  {
+    name: "ee_work",
+    catagory: "workshop_stamp",
+    done: false
+  },
+  {
+    name: "ene_norm",
+    catagory: "department_stamp",
+    done: false
+  },
+  {
+    name: "ene_work",
+    catagory: "workshop_stamp",
+    done: false
+  },
+  {
+    name: "tme_norm",
+    catagory: "department_stamp",
+    done: false
+  },
+  {
+    name: "tme_work",
+    catagory: "workshop_stamp",
+    done: false
+  },
+  {
+    name: "env_norm",
+    catagory: "department_stamp",
+    done: false
+  },
+  {
+    name: "env_work",
+    catagory: "workshop_stamp",
+    done: false
+  },
+  {
+    name: "cpe_norm",
+    catagory: "department_stamp",
+    done: false
+  },
+  {
+    name: "cpe_work",
+    catagory: "workshop_stamp",
+    done: false
+  },
+  {
+    name: "inc_norm",
+    catagory: "department_stamp",
+    done: false
+  },
+  {
+    name: "inc_work",
+    catagory: "workshop_stamp",
+    done: false
+  },
+  {
+    name: "che_norm",
+    catagory: "department_stamp",
+    done: false
+  },
+  {
+    name: "che_work",
+    catagory: "workshop_stamp",
+    done: false
+  },
+  {
+    name: "central_norm",
+    catagory: "department_stamp",
+    done: false
+  },
+  {
+    name: "central_question",
+    catagory: "special_stamp",
+    done: false
+  },
+  {
+    name: "central_innovation",
+    catagory: "special_stamp",
+    done: false
+  },
+  {
+    name: "central_hackathon",
+    catagory: "special_stamp",
+    done: false
+  },
+]
